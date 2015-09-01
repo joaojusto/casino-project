f29f9e593760ac488178be47334a97d11f766b90
João Justo <jpjustonunes@hotmail.com>
2015-09-01 22:07:27 +0100

    Adds i18n

---
+++ client/startup.js
@@ -0,0 +1,28 @@
+
+Meteor.startup(startup);
+
+function startup() {
+  loadLanguage();
+}
+
+function getUserLanguage() {
+  return "en";
+}
+
+function loadLanguage() {
+  Session.set("showLoadingIndicator", true);
+
+  function changeCompleted() {
+    Session.set("showLoadingIndicator", false);
+  }
+
+  function changeFailed() {
+    Session.set("showLoadingIndicator", false);
+    console.log(error_message);
+  }
+
+  TAPi18n
+    .setLanguage(getUserLanguage())
+    .done(changeCompleted)
+    .fail(changeFailed);
+}
